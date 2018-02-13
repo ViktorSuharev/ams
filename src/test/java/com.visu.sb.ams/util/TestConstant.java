bbe3bf91d60c1732da17831c7a892a6027b1f9ff
Viktor Sukharev <visu0215@netcracker.com>
2018-02-13 12:33:09 +0300

    set serializable level

--- src/test/java/com.visu.sb.ams/util/TestConstant.java
+++ src/test/java/com.visu.sb.ams/util/TestConstant.java
@@ -2,7 +2,7 @@ package com.visu.sb.ams.util;
 
 public class TestConstant {
 
-    public static final String H2_CONNECTION_URL = "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE";
+    public static final String H2_CONNECTION_URL = "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE;MVCC=FALSE;LOCK_MODE=1";
     public static final String H2_USER_NAME = "h2";
     public static final String H2_PASSWORD = "h2";
 
